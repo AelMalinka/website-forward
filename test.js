524aa677229a325b18627e67548b0f901e343504
Michael Thomas (malinka) <malinka@entropy-development.com>
2017-05-13 04:32:37 -0400

    use vows for testing

--- test.js
+++ test.js
@@ -5,17 +5,173 @@
 'use strict';
 
 const koa = require('koa');
-const logger = require('koa-logger');
+const me = require('./');
+
 const body = require('koa-bodyparser');
-const request = require('./');
+const request = require('request');
 
-const app = new koa();
+const vows = require('vows');
+const assert = require('assert');
 
-app.use(logger());
-app.use(body());
-app.use(request());
-app.use(async (ctx) => {
-	return await ctx.forward('http://localhost:8084/default.css');
+const baseapp = new koa();
+const forwardapp = new koa();
+
+baseapp.use(body());
+baseapp.use(async (ctx) => {
+	if(ctx.request.method === 'POST') {
+		ctx.body = 'Hello!\n';
+		ctx.body += JSON.stringify(ctx.request.body);
+		ctx.body += '\n';
+	} else {
+		ctx.body = 'Hello!\n';
+	}
 });
 
-app.listen(3000);
+forwardapp.use(body());
+forwardapp.use(me());
+forwardapp.use(async (ctx) => {
+	return await ctx.forward('http://localhost:3001/');
+});
+
+const base = baseapp.listen(3001);
+const forward = forwardapp.listen(3000);
+
+const empty = {};
+const value = {
+	value: 'test',
+	other: 'meow!',
+};
+
+vows.describe('Forwrad').addBatch({
+	'Base': {
+		topic: base,
+		'GET': {
+			topic: function () {
+				request({
+					uri: 'http://localhost:3001/',
+				}, this.callback);
+			},
+			'not errored': function(err, res, body) {
+				assert.isTrue(!err);
+			},
+			'is 200 OK': function(err, res, body) {
+				assert.equal(res.statusCode, 200);
+			},
+			'is Hello!': function(err, res, body) {
+				assert.equal(body, 'Hello!\n');
+			},
+		},
+		'empty POST': {
+			topic: function() {
+				request({
+					uri: 'http://localhost:3001/',
+					method: 'POST',
+					form: empty,
+				}, this.callback);
+			},
+			'not errored': function(err, res, body) {
+				assert.isTrue(!err);
+			},
+			'is 200 OK': function(err, res, body) {
+				assert.equal(res.statusCode, 200);
+			},
+			'is correct response': function(err, res, body) {
+				assert.equal(body, 'Hello!\n' + JSON.stringify(empty) + '\n');
+			},
+		},
+		'POST': {
+			topic: function() {
+				request({
+					uri: 'http://localhost:3001/',
+					method: 'POST',
+					form: value,
+				}, this.callback);
+			},
+			'not errored': function(err, res, body) {
+				assert.isTrue(!err);
+			},
+			'is 200 OK': function(err, res, body) {
+				assert.equal(res.statusCode, 200);
+			},
+			'is correct response': function(err, res, body) {
+				assert.equal(body, 'Hello!\n' + JSON.stringify(value) + '\n');
+
+				// 2017-05-13 AMR NOTE: objects are not equal to each other, TODO?
+				const v = JSON.parse(body.split('\n')[1]);
+				assert.equal(value.value, v.value);
+				assert.equal(value.other, v.other);
+			},
+		},
+		'Forward': {
+			topic: forward,
+			'GET': {
+				topic: function () {
+					request({
+						uri: 'http://localhost:3000/',
+					}, this.callback);
+				},
+				'not errored': function(err, res, body) {
+					assert.isTrue(!err);
+				},
+				'is 200 OK': function(err, res, body) {
+					assert.equal(res.statusCode, 200);
+				},
+				'is Hello!': function(err, res, body) {
+					assert.equal(body, 'Hello!\n');
+				},
+			},
+			'empty POST': {
+				topic: function() {
+					request({
+						uri: 'http://localhost:3000/',
+						method: 'POST',
+						form: empty,
+					}, this.callback);
+				},
+				'not errored': function(err, res, body) {
+					assert.isTrue(!err);
+				},
+				'is 200 OK': function(err, res, body) {
+					assert.equal(res.statusCode, 200);
+				},
+				'is correct response': function(err, res, body) {
+					assert.equal(body, 'Hello!\n' + JSON.stringify(empty) + '\n');
+				},
+			},
+			'POST': {
+				topic: function() {
+					request({
+						uri: 'http://localhost:3000/',
+						method: 'POST',
+						form: value,
+					}, this.callback);
+				},
+				'not errored': function(err, res, body) {
+					assert.isTrue(!err);
+				},
+				'is 200 OK': function(err, res, body) {
+					assert.equal(res.statusCode, 200);
+				},
+				'is correct response': function(err, res, body) {
+					assert.equal(body, 'Hello!\n' + JSON.stringify(value) + '\n');
+
+					// 2017-05-13 AMR NOTE: objects are not equal to each other, TODO?
+					const v = JSON.parse(body.split('\n')[1]);
+					assert.equal(value.value, v.value);
+					assert.equal(value.other, v.other);
+				},
+			},
+		},
+	}
+}).addBatch({
+	'Base': {
+		'close': function() {
+			base.close();
+		},
+	},
+	'Forward': {
+		'close': function() {
+			forward.close();
+		}
+	},
+}).run();
